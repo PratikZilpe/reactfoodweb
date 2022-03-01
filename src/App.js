12d20217f3484012a4b20a96532dbaa2cc54fec5
pratikzilpe <pratik.z@toranainc.com>
2022-03-01 20:54:16 +0530

    deploy my react app to github

--- src/App.js
+++ src/App.js
@@ -1,39 +1,22 @@
 //import { usestate, useEffect } from "react"
 import './App.css';
 import Header from './My component/Header';
-//import { useState } from 'react';
-//import {Units} from './My component/Units';
-//import {Footer} from './My component/Footer';
-//import { Categories } from './My component/Categories';
-import RecipeItem from './My component/RecipeItem';
-//import Recipes from './My component/Recipes';
-//import Meal from './My component/Meal';
-//import RecipesItems from './My component/RecipesItems';
 
-//const apiUrl = "https://www.themaldb.com/api/json/v1/1/search.php?s=";
+import RecipeItem from './My component/RecipeItem';
+
 
 function App() {
 
- // const [filteredItem, setFilteredItem] = useState('');
-
- const filteredFood = () => {
-   
- } 
 
 
   return (
     <>
     
-    <Header foodSelection={filteredFood}/>
-    {/* <Units/>
-    <Footer/>
-    <Categories/> */}
+    <Header />
+ 
     <RecipeItem />
   
 
-    {/* <Recipes></Recipes> */}
-    {/* <Meal></Meal> */}
-   
 
     </>
    
